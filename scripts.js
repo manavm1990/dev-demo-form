4a5dd3ca3721893f1f4e0875da578f2c4972b6ff
Manav Misra <manavm@visionwebsoft.com>
2020-10-31 09:04:40 -0500

    :sparkles: Collect all 'form data' into an object

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
+
+  console.log(
+    Array.from(event.target.elements)
+      .filter(({ id }) => id)
+      .reduce(
+        (accumulator, { id, value }) => ({
+          ...accumulator,
+          ...{ [id]: value },
+        }),
+        {}
+      )
+  );
 });
